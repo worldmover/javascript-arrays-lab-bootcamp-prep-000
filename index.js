f45f4fffacb2da227557447a01b680ed5e8b8835
Christian Weiss <worldmover777@gmail.com>
2018-01-27 23:35:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-kittens = ['Milo', 'Otis', 'Garfield']
-
 function destructivelyAppendKitten(name){
   kittens=kittens.push(name)
   return kittens
 }
-
-kittens = ["Milo", "Otis", "Garfield"]
-
-function dextructivelyPrependKitten(name){
-  kittens=kittens.unshift(name)
-  return kittens
-}
-
